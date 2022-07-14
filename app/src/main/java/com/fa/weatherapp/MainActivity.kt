70faf92d58360a10e53541b8e6de7525c2546615
fasol8 <34354696+Fabritzi0@users.noreply.github.com>
2022-07-13 20:01:31 -0500

    feat: Location implementation (fix)

--- app/src/main/java/com/fa/weatherapp/MainActivity.kt
+++ app/src/main/java/com/fa/weatherapp/MainActivity.kt
@@ -1,54 +1,48 @@
 package com.fa.weatherapp
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.icu.text.SimpleDateFormat
+import android.location.Location
+import android.location.LocationManager
 import android.os.AsyncTask
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.transition.AutoTransition
 import android.transition.TransitionManager
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.fa.weatherapp.databinding.ActivityMainBinding
+import com.google.android.gms.location.*
 import org.json.JSONObject
 import java.net.URL
 import java.util.*
+import java.util.jar.Manifest
 import kotlin.reflect.typeOf
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     var CITY: String = "Mexico City, MX"
     val API: String = "45541d49d6c7728b79bb57cc6b29979f"
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    var BAND = 'C'
+    var LAT = ""
+    var LON = ""
+    val PERMISSION_ID = 42
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        weather().execute()
-    }
-
-    fun expand(view: View) {
-        if (binding.layoutDetails.visibility == View.GONE) {
-            TransitionManager.beginDelayedTransition(binding.cardView, AutoTransition())
-            binding.layoutDetails.visibility = View.VISIBLE
-        } else {
-            TransitionManager.beginDelayedTransition(binding.cardView, AutoTransition())
-            binding.layoutDetails.visibility = View.GONE
-        }
-    }
-
-    fun update(view: View) {
-        val ocity = binding.cityText.text
-        if (ocity == null) {
-            CITY = "Mexico City, MX"
-            Toast.makeText(applicationContext, "Escriba una ciudad", Toast.LENGTH_LONG).show()
-        } else {
-            CITY = ocity.toString()
-        }
-        Toast.makeText(applicationContext, "Es la ciudad " + CITY, Toast.LENGTH_LONG).show()
         weather().execute()
     }
 
@@ -63,10 +57,21 @@ class MainActivity : AppCompatActivity() {
         override fun doInBackground(vararg p0: String?): String? {
             var response: String?
             try {
-                response =
-                    URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&lang=es&appid=$API").readText(
-                        Charsets.UTF_8
-                    )
+                if (BAND == 'C') {
+                    response =
+                        URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&lang=es&appid=$API").readText(
+                            Charsets.UTF_8
+                        )
+                    println(BAND)
+                    println("Entro en C")
+                } else if (BAND == 'L') {
+                    response =
+                        URL("https://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON&units=metric&lang=es&appid=$API").readText(
+                            Charsets.UTF_8
+                        )
+                } else {
+                    response = null
+                }
             } catch (e: Exception) {
                 response = null
             }
@@ -117,6 +122,9 @@ class MainActivity : AppCompatActivity() {
                 binding.humidity.text = humidity
                 binding.windD.text = windDeg
                 binding.cloud.text = clouding
+                Toast.makeText(applicationContext, "Bandera " + BAND, Toast.LENGTH_SHORT).show()
+                BAND = 'C'
+
 //                binding.imageWeather.imageAlpha =
 
                 binding.loader.visibility = View.GONE
@@ -126,5 +134,154 @@ class MainActivity : AppCompatActivity() {
                 binding.errorText.visibility = View.VISIBLE
             }
         }
+    }
+
+    fun expand(view: View) {
+        if (binding.layoutDetails.visibility == View.GONE) {
+            TransitionManager.beginDelayedTransition(binding.cardView, AutoTransition())
+            binding.layoutDetails.visibility = View.VISIBLE
+        } else {
+            TransitionManager.beginDelayedTransition(binding.cardView, AutoTransition())
+            binding.layoutDetails.visibility = View.GONE
+        }
+    }
+
+    fun update(view: View) {
+        val ocity = binding.cityText.text
+        if (ocity == null) {
+            CITY = "Mexico City, MX"
+            Toast.makeText(applicationContext, "Escriba una ciudad", Toast.LENGTH_LONG).show()
+        } else {
+            CITY = ocity.toString()
+        }
+        Toast.makeText(applicationContext, "Es la ciudad " + CITY, Toast.LENGTH_LONG).show()
+        BAND = 'C'
+        weather().execute()
+    }
+
+    fun location(view: View) {
+        //Preguntar si se tiene permisos
+        if (allPermissionsGrantedGPS()) {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        } else {
+            // Si no hay permisos solicitarlos al usuario
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                PERMISSION_ID
+            )
+        }
+        leerubicacionactual()
+        weather().execute()
+    }
+
+    private fun allPermissionsGrantedGPS() = REQUIRED_PERMISSIONS_GPS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun leerubicacionactual() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                        var location: Location? = task.result
+                        if (location == null) {
+                            requestNewLocationData()
+                        } else {
+                            LAT = location.latitude.toString()
+                            LON = location.longitude.toString()
+                            BAND = 'L'
+                            Toast.makeText(
+                                this,
+                                "-Latitud: " + LAT + " -Longitud: " + LON + " Bandera " + BAND,
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                        }
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Activar ubicación", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+                this.finish()
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                PERMISSION_ID
+            )
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest,
+            mLocationCallBack,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallBack = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location = locationResult.lastLocation
+            LAT = mLastLocation.latitude.toString()
+            LON = mLastLocation.longitude.toString()
+            BAND = 'L'
+            Toast.makeText(
+                applicationContext, "Latitud: " + LAT + " Longitud: " + LON, Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS_GPS = arrayOf(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
     }
 }
